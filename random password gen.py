f9d5797a949f0a2abb0b2282fafb1860241db4cc
astroSnoo <102867466+astroSnoo@users.noreply.github.com>
2022-04-02 20:12:03 +0100

    Add files via upload

---
+++ random password gen.py
@@ -0,0 +1,11 @@
+import random
+
+characters = "abcdefghijklmnopqrstuvwxyz$%!^&*()?:;@#1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+characters = list(set(characters))
+amount = int(input("How many passwords do you want to generate?\n"))
+length = int(input("How many characters would you like your pass word to be?\n"))
+for i in range(amount):
+    password = ""
+    for x in range(15):
+        password += random.choice(characters)
+    print(f"Your new password is {password}")
