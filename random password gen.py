5f96deb45663185eedca1ee012348c3ac2dd64e2
astroSnoo <102867466+astroSnoo@users.noreply.github.com>
2022-04-02 20:16:25 +0100

    Update random password gen.py

--- random password gen.py
+++ random password gen.py
@@ -6,6 +6,6 @@ amount = int(input("How many passwords do you want to generate?\n"))
 length = int(input("How many characters would you like your pass word to be?\n"))
 for i in range(amount):
     password = ""
-    for x in range(15):
+    for x in range(length):
         password += random.choice(characters)
     print(f"Your new password is {password}")
